a931dd812e945904d18e3faf6adf0ec36831f751
cannell001 <cannell001@gannon.edu>
2018-09-06 14:52:21 -0400

    use map to add <li>...</li> around item

--- src/TodoComponent.js
+++ src/TodoComponent.js
@@ -4,23 +4,25 @@ class TodoComponent extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-            todos: ['wash the dishes', 'cut the cheese', 'take a nap']
-        };
-    }
-    //getInitialState
-    render(){
-        return(
-            <div id="todo-list">
-				<h2>State with JSX</h2>
-                <p>React stuff goes here...</p>
-				<ul>
-					<li>{this.state.todos[0]}</li>
-					<li>{this.state.todos[1]}</li>
-					<li>{this.state.todos[2]}</li>
-				</ul>
-            </div>
-        );
-    } //render
+			todos: ['walk the dog', 'drink tea', 'wash the dishes', 'cut the cheese', 'take a nap']
+		};
+	}
+	//getInitialState
+	render(){
+		var todos = this.state.todos;
+		todos = todos.map(function(item, index){
+			return(
+					<li>{item}</li>
+			);
+		});
+		return(
+			<div id="todo-list">
+				<h2>State 2 w/ JSX</h2>
+				<p>React stuff goes here...</p>
+				<ul>{todos}</ul>
+			</div>
+		);
+	} //render
 }
 
 export default TodoComponent;
