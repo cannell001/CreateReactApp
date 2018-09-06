e30ad3a7ec165b88fe27837279065a7d19105b87
cannell001 <cannell001@gannon.edu>
2018-09-06 12:50:08 -0400

    Props 1

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+        <div>
+            <h1>Using ES6 Classes with props</h1>
+            <p>{this.props.msg}</p>
+        </div>
     );
   }
 }
