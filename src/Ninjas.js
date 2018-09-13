8088e9a5777d69c1529b85bc7c40b11e69f704d7
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-13 11:26:54 -0400

    if else with ternary

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,22 +1,28 @@
 import React from 'react'
 
 const Ninjas = ({ ninjas }) => {
-  // const { ninjas } = this.props;
-  //const { ninjas } = props; destructure ninjas from props
+  // const ninjaList = ninjas.map(ninja => {
+  //     if (ninja.age > 20){
+  //       return (
+  //         <div className="ninja" key={ninja.id}>
+  //           <div>Name: { ninja.name }</div>
+  //           <div>Age: { ninja.age }</div>
+  //           <div>Belt: { ninja.belt }</div>
+  //         </div>
+  //         )
+  //       } else {
+  //           return null;
+  //       }
+  // })
   const ninjaList = ninjas.map(ninja => {
-      if (ninja.age > 20){
-        return (
+      return ninja.age > 20 ? (
           <div className="ninja" key={ninja.id}>
             <div>Name: { ninja.name }</div>
             <div>Age: { ninja.age }</div>
             <div>Belt: { ninja.belt }</div>
-          </div>
-          )
-        } else {
-            return null;
-        }
+         </div>
+     ) : null;
   })
-
   return (
     <div className="ninja-list">
       { ninjaList }
