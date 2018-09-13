00d88cf8f25ab29cac9778dee81dcb09515ff21c
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-13 11:18:12 -0400

    if else conditional

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -4,14 +4,18 @@ const Ninjas = ({ ninjas }) => {
   // const { ninjas } = this.props;
   //const { ninjas } = props; destructure ninjas from props
   const ninjaList = ninjas.map(ninja => {
-    return (
-      <div className="ninja" key={ninja.id}>
-        <div>Name: { ninja.name }</div>
-        <div>Age: { ninja.age }</div>
-        <div>Belt: { ninja.belt }</div>
-      </div>
-    )
-  });
+      if (ninja.age > 20){
+        return (
+          <div className="ninja" key={ninja.id}>
+            <div>Name: { ninja.name }</div>
+            <div>Age: { ninja.age }</div>
+            <div>Belt: { ninja.belt }</div>
+          </div>
+          )
+        } else {
+            return null;
+        }
+  })
 
   return (
     <div className="ninja-list">
